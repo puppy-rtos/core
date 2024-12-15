9b692a5cb35aa2d938a4b53fce22da4763642de7
guozhanxin <guozhanxin@rt-thread.com>
2024-12-15 09:04:13 +0800

    add riscv

--- xmake.lua
+++ xmake.lua
@@ -2,11 +2,11 @@
 target("puppy")
 
     add_defines('PUPPY_RTOS')
-    add_files("*.c", {cxflags = "-Wall -Werror --coverage"})
+    add_files("*.c", {cxflags = "-Wall -Werror"})
     add_includedirs(".")
 
 if is_config("build_board", "qemu-virt-riscv") then
-    add_files("arch/**.c")
+    add_files("arch/arch_riscv.c")
 else
-    add_files("arch/**.c")
+    add_files("arch/arch_cortex-m.c")
 end
